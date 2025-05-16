3ad9079d8a24360403457812a81b9de5b62ca6f7
ormor <htth815@gmail.com>
2025-05-16 10:01:45 +0900

    ⬆️ next/mdx 관련 패키지 추가 - @mdx-js/loader, @mdx-js/react, @next/mdx, @types/mdx, dayjs, @tailwindcss/typography, @types/glob, glob, gray-matter, katex, prismjs, rehype-autolink-headings, rehype-codetitles, rehype-katex, rehype-prism-plus, rehype-slug, remark-breaks, remark-gfm, remark-math, remark-toc

--- src/constants/Navbar.ts
+++ src/constants/Navbar.ts
@@ -3,8 +3,9 @@
  */
 
 export const NAV_ITEMS = [
-  { name: 'Stories', path: '/stories' }, // 나의 이야기와 경험, 인사이트 기록
-  { name: 'Notes', path: '/notes' },  // 배우고 익힌 지식 공유 및 기록
+  { name: 'Posts', path: '/posts' }, // 나의 이야기와 경험, 인사이트 기록
+//  { name: 'Stories', path: '/stories' }, // 나의 이야기와 경험, 인사이트 기록
+//  { name: 'Notes', path: '/notes' },  // 배우고 익힌 지식 공유 및 기록
 //   { name: 'Archive', path: '/archive' }, // 모든 태그, 게시물 조회
 //   { name: 'Algorithms', path: '/algorithms' }, // 알고리즘, 코테 관련 기록
 //   { name: 'Projects', path: '/projects' }, // 1 to 10 프로젝트 따라하기
